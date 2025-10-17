a499064344628384236f6223bf34a87bf49cdd76
Nikhil Bhagoria <nikhilkumar2450@gmail.com>
2025-10-17 23:13:56 +0530

    Add Theme and Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import { useState } from 'react'
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-
+import theme from './theme';
 import './App.css'
+import Login from './pages/Login';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +15,8 @@ function App() {
         <CssBaseline />
         <BrowserRouter>
           <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route path="/login"  element={<Login />} />
+            <Route path='*' element={<Login/>}/>
 
           </Routes>
         </BrowserRouter>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  });
+  const LoginHandler = (e) => {
+    e.preventDefault();
+    console.log('Logging in with', credentials);
+    // Add your login logic here
+  }
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={LoginHandler}>
+      <input type="text" placeholder="Username"
+        value={credentials.username}
+        onChange={(e) => setCredentials({ ...credentials, username: e.target.value })} />
+      <input type="password" placeholder="Password"
+        value={credentials.password}
+        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })} />
+      <button>Login</button>
+      </form>
+    </div>
   )
 }
 
