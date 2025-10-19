8ab051874e3bbd62d7c146bbf7e26a93c7f0e3db
Nikhil Bhagoria <nikhilkumar2450@gmail.com>
2025-10-19 23:36:26 +0530

    Add: Design Register Page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Register = () => {
   return (
     <div>
-        <form onSubmit={SubmitHandler}>
-        <input type="text" placeholder="Username" />
+        <form onSubmit={SubmitHandler} classname="flex flex-col gap-4 w-1/3 mx-auto mt-10 p-6 border border-gray-300 rounded-lg shadow-lg bg-white">
+        <input type="text" placeholder="Username" className='' />
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
         <button type='submit'>Register</button>
