16dba95f4463ee892c7973d970cc40f842801c46
Nikhil Bhagoria <nikhilkumar2450@gmail.com>
2025-10-20 23:13:15 +0530

    Update: Add Material form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,22 @@
+import TextField from '@mui/material/TextField';
 import React from 'react'
 
 const Register = () => {
+   const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  }
   return (
     <div>
-        <form onSubmit={SubmitHandler} classname="flex flex-col gap-4 w-1/3 mx-auto mt-10 p-6 border border-gray-300 rounded-lg shadow-lg bg-white">
-        <input type="text" placeholder="Username" className='' />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <form onSubmit={SubmitHandler} classname="flex gap-4 w-1/3 mx-auto mt-10 p-6 border border-gray-300 rounded-lg shadow-lg bg-white">
+        <TextField id="standard-basic" required label="Username" variant="standard" />
+        <TextField id="standard-basic" required label="Email" variant="standard" />
+        <TextField id="standard-basic" required label="Password" variant="standard" />
+
         <button type='submit'>Register</button>
         </form>
     </div>
